9564e8a1ea414cb62815f1e45b7f45bbb46565e1
Chris Philbin <chris@chrisphilbin.net>
2021-02-26 07:47:40 -0500

    Creating function that will make an HTTP get request using axios every 12000 miliseconds to each defined site URL. Need to refactor to utilize useState setter method in order to properly re-render the correct DOM elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
 
-  let [googleStatus, setGoogleStatus] = useState({status: 0, count: 0})
+  let sites = [
+    {
+      name: 'google',
+      attributes: {
+        url: 'https://www.google.com',
+        isDown: false,
+        downCount: 0,
+        status: 0,
+        total: 0
+      }
+    },
+    {
+      name: 'yahoo',
+      attributes: {
+        url: 'https://www.yahoo.com',
+        isDown: false,
+        downCount: 0,
+        status: 0,
+        total: 0
+      }
+    },
+    {
+      name: 'ultimate',
+      attributes: {
+        url: 'https://www.ultimatesoftware.com',
+        isDown: false,
+        downCount: 0,
+        status: 0,
+        total: 0
+      }
+    }
+  ]
 
-  const monitorkUpTime = (url, stats, setter) => {
-    axios.get('https://sleepy-plateau-48238.herokuapp.com/' + url)
-    .then(response => setter({...stats, status: response.status}))
+  let [monitoredSites, setMonitoredSites] = useState(sites)
+  let [isMonitoring, setIsMonitoring]     = useState(false)
+
+  //component does not properly re-render because state is not being updated
+  //using #setMonitoredSites - need to re-factor code in order to utilize setMonitoredSites
+
+  const monitorUpTime = () => {
+    sites.map((site) => {
+      axios.get('https://sleepy-plateau-48238.herokuapp.com/' + site.url)
+      .then(response => site.status = response.status)
+      // .then(response => setMonitoredSites({...monitoredSites, }))
+      .then(site.total += 1)
+      .then(console.log(site, "site object"))
+    })
   }
 
-  console.log(googleStatus, "google status")
+  const beginMonitoring = () => {
+    setIsMonitoring(true)
+    setInterval(monitorUpTime, 12000)
+  }
 
-  return(
-    <div>
-      Hello from React
-      <p onClick={() => monitorUpTime('https://www.google.com', googleStatus, setGoogleStatus)}>Click me!</p>
-    </div>
-  )
+  if (!isMonitoring) {
+    return(
+      <div>
+        <p onClick={() => beginMonitoring()}>Click me!</p>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        Started monitoring...
+        {
+          monitoredSites.map((site) => (
+            <li key={site.name}>{site.name} - {site.status}</li>
+          ))
+        }
+      </div>
+    )
+  }
 }
 
 export default App
