04046c7f47d86bf495ff5fb2dcd51f8929b3da34
Chris Philbin <chris@chrisphilbin.net>
2021-02-28 11:47:59 -0500

    Adding additional conditional logic to account for when a site remains down and subsequent get requests shouldn't be sent until the user manually resets the status of isDown

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -32,7 +33,7 @@ const App = () => {
     {
       name: 'reddit',
       attributes: {
-        url: 'https://www.reddit.com',
+        url: 'http://www.reddit.com',
         isDown: false,
         downCount: 0,
         httpStatusCode: 0,
@@ -56,10 +57,15 @@ const App = () => {
 
    const checkUpTime = () => {
     monitoredSites.forEach((site) => {
+      if (site.attributes.isDown === true) {
+        return
+      }
+      console.log(site, "current site object being checked")
       fetch('https://sleepy-plateau-48238.herokuapp.com/' + site.attributes.url)
       .then(response => {
         if (response.status === 200) {
           site.attributes.httpStatusCode = response.status
+          site.attributes.total += 1
           setMonitoredSites([...monitoredSites])
         } else {
           site.attributes.httpStatusCode = response.status
@@ -77,20 +83,29 @@ const App = () => {
   const beginMonitoring = () => {
     setIsMonitoring(true)
     checkUpTime()
-    setInterval(checkUpTime, 15000)
+    setInterval(checkUpTime, 5000)
+  }
+
+  const resetIsDown = (site) => {
+    site.attributes.isDown = false
+    site.attributes.downCount = 0
+    setMonitoredSites([...monitoredSites])
   }
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <button onClick={() => beginMonitoring()}>Begin</button>
+          <Button variant="contained" color="primary" onClick={ () => beginMonitoring() }>
+            Start Monitoring
+          </Button>
         </Grid>
         {
           monitoredSites.map((site) => (
             <Grid item xs={6}>
               <Paper className={classes.paper}>
                 <li key={site.name}>{site.name} - {site.attributes.httpStatusCode} - {site.attributes.total} - {site.attributes.downCount}</li>
+                {site.attributes.isDown ? <Button variant="contained" color="primary" onClick={ () => resetIsDown(site)}>Start monitoring</Button> : null }
               </Paper>
             </Grid>
           ))
